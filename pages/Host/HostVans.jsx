56ac132493a6fb2d9f5deef5c27e51e75877b910
shobhitt7 <shobhitgs2011@gmail.com>
2025-05-14 13:37:38 +0530

    bug fixing

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -32,7 +32,7 @@ export default function HostVans() {
                 <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
                 <div className="host-van-info">
                     <h3>{van.name}</h3>
-                    <p>₹{van.price}/day</p>
+                    <p>${van.price}/day</p>
                 </div>
             </div>
         </Link>
